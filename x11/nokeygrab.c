e188097828c4020bbcc06d9537337ddc2f580424
Daniel Scharrer <daniel@constexpr.org>
2012-12-29 10:50:17 +0100

    Update nokeygrab.c
    * Change license to WTFPL
    * Compile both 32-bit and 64-bit libraries
    * Print name of attached process
    * Add in-source build script

--- x11/nokeygrab.c
+++ x11/nokeygrab.c
@@ -1,34 +1,54 @@
-/*
- * Copyright (C) 2012 Daniel Scharrer
- *
- * This software is provided 'as-is', without any express or implied
- * warranty.  In no event will the author(s) be held liable for any damages
- * arising from the use of this software.
- *
- * Permission is granted to anyone to use this software for any purpose,
- * including commercial applications, and to alter it and redistribute it
- * freely, subject to the following restrictions:
- *
- * 1. The origin of this software must not be misrepresented; you must not
- *    claim that you wrote the original software. If you use this software
- *    in a product, an acknowledgment in the product documentation would be
- *    appreciated but is not required.
- * 2. Altered source versions must be plainly marked as such, and must not be
- *    misrepresented as being the original software.
- * 3. This notice may not be removed or altered from any source distribution.
- */
+#if 0
 
-/*
- * Library that can be used to prevent (SDL) games or other Xlib clients from
- * actively grabbing the keyboard.
- *
- * Compile:
- *  $ gcc -shared -fPIC nokeygrab.c -o nokeygrab.so
- * Add -m32 for use with 32-bit binaries
- *
- * Use:
- *  $ LD_PRELOAD="$(pwd)/nokeygrab.so" sdl-program
- */
+#// Library that can be used to prevent (SDL) games or other Xlib clients from
+#// actively grabbing the keyboard.
+#//
+#//
+#// Requires: gcc with support for x86 targets, Xlib + headers
+#//
+#//
+#// Use:
+#// $ chmod +x nokeygrab.c
+#// and then
+#//
+#//
+#// $ ./nokeygrab.c <program>
+#//
+#//
+#// This program is free software. It comes without any warranty, to
+#// the extent permitted by applicable law. You can redistribute it
+#// and/or modify it under the terms of the Do What The Fuck You Want
+#// To Public License, Version 2, as published by Sam Hocevar. See
+#// http://sam.zoy.org/wtfpl/COPYING for more details.
+#//
+
+self="$(readlink -f "$(which "$0")")"
+out="$(dirname "$self")/build"
+name="$(basename "$self" .c).so"
+
+
+#// Compile the LD_PRELOAD library
+for arch in 32 64 ; do
+	dir="$out/$arch"
+	lib="$dir/$name"
+	mkdir -p "$dir"
+	if [ "$self" -nt "$lib" ] ; then
+		echo "Compiling $arch-bit $name..."
+		gcc -shared -fPIC -m$arch "$self" -o "$lib" \
+		    -lX11 -static-libgcc -O3 -Wall -Wextra \
+			|| echo "Warning: could not compile $arch-bit library"
+	fi
+done
+
+#// Run the executable
+export LD_LIBRARY_PATH="$out/32:$out/64"
+export LD_PRELOAD="$name"
+[ -z "$1" ] || exec "$@"
+
+
+exit
+
+#endif // 0
 
 #include <stdio.h>
 #include <string.h>
@@ -36,24 +56,34 @@
 
 #define PREVENT_POINTER_GRAB 0
 
+extern char * program_invocation_short_name; // provided by glibc
+
 /*! XGrabKeyboard replacement that ignores all requests */
 int XGrabKeyboard(Display * display, Window grab_window, Bool owner_events,
                   int pointer_mode, int keyboard_mode, Time time) {
-	fprintf(stderr, "[nokeygrab] prevented keyboard grab\n");
+	(void)display, (void)grab_window, (void) owner_events;
+	(void)pointer_mode, (void)keyboard_mode, (void)time;
+	fprintf(stderr, "[nokeygrab] prevented keyboard grab in %s\n",
+	        program_invocation_short_name);
 	return GrabSuccess;
 }
 
 #if PREVENT_POINTER_GRAB
+/*! XGrabPointer replacement that ignores all requests */
 int XGrabPointer(Display * display, Window grab_window, Bool owner_events,
                  unsigned int event_mask, int pointer_mode, int keyboard_mode,
                  Window confine_to, Cursor cursor, Time time) {
-	fprintf(stderr, "[nokeygrab] prevented pointer grab\n");
+	(void)display, (void)grab_window, (void) owner_events;
+	(void)event_mask, (void)pointer_mode, (void)keyboard_mode;
+	(void)confine_to, (void)cursor, (void)time;
+	fprintf(stderr, "[nokeygrab] prevented pointer grab in %s\n",
+	        program_invocation_short_name);
 	return GrabSuccess;
 }
 #endif
 
 /*
- * SDL loads all Xlib functions dynamically
+ * Some programs (e.g. those using SDL) load all Xlib functions dynamically
  * We need to intercept those dlsym calls and inject our own XGrabKeyboard
  */
 
@@ -63,13 +93,15 @@ void * __libc_dlsym(void * handle, const char * name);
 void * dlsym(void * handle, const char * name) {
 	
 	if(!strcmp(name, "XGrabKeyboard")) {
-		fprintf(stderr, "[nokeygrab] dlsym XGrabKeyboard\n");
+		fprintf(stderr, "[nokeygrab] dlsym XGrabKeyboard in %s\n",
+		        program_invocation_short_name);
 		return XGrabKeyboard;
 	}
 	
 	#if PREVENT_POINTER_GRAB
 	if(!strcmp(name, "XGrabPointer")) {
-		fprintf(stderr, "[nokeygrab] dlsym XGrabPointer\n");
+		fprintf(stderr, "[nokeygrab] dlsym XGrabPointer in %s\n",
+		        program_invocation_short_name);
 		return XGrabPointer;
 	}
 	#endif
@@ -82,5 +114,5 @@ void * dlsym(void * handle, const char * name) {
 void init_nokeygrab(void) __attribute__((constructor));
 
 void init_nokeygrab(void) {
-	fprintf(stderr, "[nokeygrab] attached to process\n");
+	fprintf(stderr, "[nokeygrab] attached to %s\n", program_invocation_short_name);
 }
