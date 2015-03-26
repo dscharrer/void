dd1721a380cf254f74c53fe5f64247c78d7937f3
Daniel Scharrer <daniel@constexpr.org>
2015-03-26 19:23:16 +0100

    Add GL_ATI_meminfo hack for MESA

---
+++ hacks/glamdmeminfo.c
@@ -0,0 +1,296 @@
+#if 0
+
+#/*
+# * Fix for games/engines that require NV/AMD OpenGL extensions to query VRAM
+# * size and behave stupidly when those are not available.
+# * This implementation requires the GLX_MESA_query_renderer extension.
+# */
+
+self="$(command -v "$0")" ; self="${self:-$0}" ; self="$(readlink -f "$self")"
+name="${self##*/}" ; name="${name%.c}"
+out="${self%/*}/$name"
+soname="$name.so"
+
+#/* Compile the LD_PRELOAD library */
+for arch in 32 64 ; do
+	mkdir -p "$out/$arch"
+	if [ ! -f "$out/$arch/$soname" ] || [ "$self" -nt "$out/$arch/$soname" ] ; then
+		echo "Compiling $arch-bit $soname..."
+		export HACK_SOURCE="$self"
+		export HACK_LIB="$out/$arch/$soname"
+		export PKG_CONFIG_PATH="/usr/lib$arch/pkgconfig:/usr/lib/pkgconfig"
+		case $arch in
+			32) export PKG_CONFIG_PATH="/usr/lib/i386-linux-gnu/pkgconfig/:$PKG_CONFIG_PATH" ;;
+			64) export PKG_CONFIG_PATH="/usr/lib/x86_64-linux-gnu/pkgconfig/:$PKG_CONFIG_PATH" ;;
+		esac
+		command="${CC:-gcc} -shared -std=c99 -fPIC -m$arch -x c -O3 -Wall -Wextra"
+		command="$command \"\$HACK_SOURCE\" -o \"\$HACK_LIB\""
+		command="$command $(pkg-config --cflags --libs gl)"
+		eval "$command" || exit 1
+	fi
+	export LD_LIBRARY_PATH="$out/$arch:$LD_LIBRARY_PATH"
+done
+
+#/* Run the executable */
+export LD_PRELOAD="$soname:$LD_PRELOAD"
+[ -z "$1" ] || exec "$@"
+
+exit
+
+#endif /* 0 */
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <dlfcn.h>
+#include <GL/gl.h>
+
+// "Fix" ARB shaders
+
+typedef void(*funcptr_t)();
+typedef funcptr_t (*glXGetProcAddress_t)(const GLubyte * procName);
+static glXGetProcAddress_t real_glXGetProcAddress;
+
+static void * get_proc(const char * name) {
+	
+	if(!real_glXGetProcAddress) {
+		return NULL;
+	}
+	
+	return real_glXGetProcAddress((const GLubyte *)name);
+}
+
+
+#define GL_VBO_FREE_MEMORY_ATI           0x87FB
+#define GL_TEXTURE_FREE_MEMORY_ATI       0x87FC
+#define GL_RENDERBUFFER_FREE_MEMORY_ATI  0x87FD
+
+#define GLX_RENDERER_VIDEO_MEMORY_MESA   0x8187
+
+static GLint extension_hijack_index = 0;
+
+static const char * extension_hijack[] = {
+	"GL_ATI_meminfo" //, "GL_NVX_gpu_memory_info"
+};
+
+typedef void (*glGetIntegerv_t)(GLenum  pname,  GLint *  params);
+typedef int (*glXQCRIMESA_t)(int attribute, unsigned int * value);
+
+void glGetIntegerv(GLenum pname, GLint * params) {
+	
+	switch(pname) {
+		case GL_VBO_FREE_MEMORY_ATI:
+		case GL_TEXTURE_FREE_MEMORY_ATI:
+		case GL_RENDERBUFFER_FREE_MEMORY_ATI: {
+			unsigned free = 0;
+			glXQCRIMESA_t real_glXQCRIMESA
+				= (glXQCRIMESA_t)get_proc("glXQueryCurrentRendererIntegerMESA");
+			if(real_glXQCRIMESA) {
+				(void)real_glXQCRIMESA(GLX_RENDERER_VIDEO_MEMORY_MESA, &free);
+			}
+			free *= 1024; // MiB -> KiB
+			params[0] = free; // total memory free in the pool
+			params[1] = free; // largest available free block in the pool
+			params[2] = free; // total auxiliary memory free
+			params[3] = free; // largest auxiliary free block
+			printf("[hook] fixed AMD_meminfo 0x%08x = %d\n", (int)pname, (int)params[0]);
+			break;
+		}
+		case GL_NUM_EXTENSIONS: {
+			glGetIntegerv_t real_glGetIntegerv = (glGetIntegerv_t)get_proc("glGetIntegerv");
+			if(real_glGetIntegerv) {
+				real_glGetIntegerv(GL_NUM_EXTENSIONS, &extension_hijack_index);
+			}
+			params[0] = extension_hijack_index + 2;
+			printf("[hook] fixed GL_NUM_EXTENSIONS​ = %d\n", (int)params[0]);
+			break;
+		}
+		default: {
+			glGetIntegerv_t real_glGetIntegerv = (glGetIntegerv_t)get_proc("glGetIntegerv");
+			if(real_glGetIntegerv) {
+				real_glGetIntegerv(pname, params);
+			}
+		}
+	}
+	
+}
+
+typedef const GLubyte * (*glGetStringi_t)(GLenum name, GLuint index);
+
+const GLubyte * glGetStringi(GLenum name, GLuint index) {
+	
+	unsigned n = sizeof(extension_hijack)/sizeof(*extension_hijack);
+	if(name == GL_EXTENSIONS && index >= (GLuint)extension_hijack_index
+	   && index <= (GLuint)extension_hijack_index + n) {
+		const char * str = extension_hijack[index - extension_hijack_index];
+		printf("[hook] fixed GL_EXTENSIONS​[%d] = %s\n", (int)index, str);
+		return (const GLubyte *)str;
+	}
+	
+	glGetStringi_t real_glGetStringi = (glGetStringi_t)get_proc("glGetStringi");
+	return real_glGetStringi ? real_glGetStringi(name, index) : NULL;
+}
+
+typedef const GLubyte * (*glGetString_t)(GLenum name);
+
+const GLubyte * glGetString(GLenum name) {
+	
+	glGetString_t real_glGetString = (glGetString_t)get_proc("glGetString");
+	const GLubyte * str = (real_glGetString ? real_glGetString(name) : NULL);
+	
+	if(str && name == GL_EXTENSIONS) {
+		unsigned n = sizeof(extension_hijack)/sizeof(*extension_hijack);
+		unsigned size = 0;
+		for(unsigned i = 0; i < n; i++) {
+			size += 2 + strlen(extension_hijack[i]);
+		}
+		if(size) {
+			unsigned m = strlen((const char *)str);
+			GLubyte * fixed = malloc(m + size + 1);
+			memcpy(fixed, str, m);
+			for(unsigned i = 0; i < n; i++) {
+				fixed[m++] = ',';
+				fixed[m++] = ' ';
+				unsigned en = strlen(extension_hijack[i]);
+				memcpy(fixed + m, extension_hijack[i], en);
+				m += en;
+			}
+			fixed[m] = '\0';
+			str = fixed; // leak
+		}
+	}
+	
+	return str;
+}
+
+// Hooking boilerplate code follows
+
+typedef void * (*dlsym_t)(void * handle, const char * name);
+static dlsym_t real_dlsym;
+
+typedef void * (*dlvsym_t)(void *handle, const char * symbol, const char * version);
+static dlvsym_t real_dlvsym;
+
+static glXGetProcAddress_t real_glXGetProcAddressARB;
+
+/*! internal libc function for the original behavior of dlsym */
+#define __RTLD_DLOPEN 0x80000000
+#define __libc_dlopen(name) \
+	__libc_dlopen_mode (name, RTLD_LAZY | __RTLD_DLOPEN)
+void * __libc_dlopen_mode(const char *__name, int __mode);
+void * __libc_dlsym(void * handle, const char * name);
+
+void hook_init(void) __attribute__((constructor));
+void hook_init(void) {
+	// Get the original dlsym (__libc_dlsym doesn't support RTLD_NEXT)
+	void * libdl = __libc_dlopen("libdl.so.2");
+	if(libdl) {
+		real_dlsym = (dlsym_t)__libc_dlsym(libdl, "dlsym");
+	}
+	if(real_dlsym) {
+		// Get the dlsym from any LD_PRELOAD library loaded before us
+		void * ptr = real_dlsym(RTLD_NEXT, "dlsym");
+		if(ptr) {
+			real_dlsym = (dlsym_t)ptr;
+		}
+		// Get other overwritten symbols
+		real_dlvsym = real_dlsym(RTLD_NEXT, "dlvsym");
+		real_glXGetProcAddress
+			= (glXGetProcAddress_t)real_dlsym(RTLD_NEXT, "glXGetProcAddress");
+		real_glXGetProcAddressARB
+			= (glXGetProcAddress_t)real_dlsym(RTLD_NEXT, "glXGetProcAddressARB");
+		if(!real_glXGetProcAddress) {
+			real_glXGetProcAddress = real_glXGetProcAddressARB;
+		}
+	}
+	printf("[hook] attached %p %p %p %p\n", real_dlsym, real_dlvsym,
+		real_glXGetProcAddress, real_glXGetProcAddressARB);
+}
+
+void * my_dlsym(const char * name, const char * hook);
+
+void * dlsym(void * handle, const char * name) {
+	
+	void * ptr = my_dlsym(name, "dlsym");
+	if(ptr) {
+		return ptr;
+	}
+	
+	if(real_dlsym) {
+		return real_dlsym(handle, name);
+	} else {
+		return NULL;
+	}
+}
+
+
+void * dlvsym(void * handle, const char * symbol, const char * version) {
+	
+	void * ptr = my_dlsym(symbol, "dlvsym");
+	if(ptr) {
+		return ptr;
+	}
+	
+	if(real_dlvsym) {
+		return real_dlvsym(handle, symbol, version);
+	} else {
+		return NULL;
+	}
+}
+
+funcptr_t glXGetProcAddress(const GLubyte * procName) {
+	
+	void * ptr = my_dlsym((const char *)procName, "glXGetProcAddress");
+	if(ptr) {
+		return (funcptr_t)ptr;
+	}
+	
+	if(real_glXGetProcAddress) {
+		return real_glXGetProcAddress(procName);
+	} else {
+		return NULL;
+	}
+}
+
+funcptr_t glXGetProcAddressARB(const GLubyte * procName) {
+	
+	void * ptr = my_dlsym((const char *)procName, "glXGetProcAddressARB");
+	if(ptr) {
+		return (funcptr_t)ptr;
+	}
+	
+	if(real_glXGetProcAddressARB) {
+		return real_glXGetProcAddressARB(procName);
+	} else if(real_glXGetProcAddress) {
+		return real_glXGetProcAddress(procName);
+	} else {
+		return NULL;
+	}
+}
+
+void * my_dlsym(const char * name, const char * hook) {
+	
+	if(name) {
+		if(!strcmp(name, "glGetIntegerv")) {
+			printf("[hook] hooked %s via %s\n", name, hook);
+			return (void *)glGetIntegerv;
+		} if(!strcmp(name, "glGetStringi")) {
+			printf("[hook] hooked %s via %s\n", name, hook);
+			return (void *)glGetStringi;
+		} if(!strcmp(name, "glGetString")) {
+			printf("[hook] hooked %s via %s\n", name, hook);
+			return (void *)glGetString;
+		} else if(!strcmp(name, "glXGetProcAddress")) {
+			return (void *)glXGetProcAddress;
+		} else if(!strcmp(name, "glXGetProcAddressARB")) {
+			return (void *)glXGetProcAddressARB;
+		}
+	}
+	
+	return NULL;
+}
